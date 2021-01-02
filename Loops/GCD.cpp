f728bd25af0f53d3c46e5115aba9c09518b7f53c
Nitesh Yadav <55007875+Niteshyadav0331@users.noreply.github.com>
2021-01-02 17:50:53 +0530

    Add files via upload

---
+++ Loops/GCD.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m, n;
+
+    cout<<"Enter two numbers: ";
+    cin>>m>>n;
+
+    while(m != n)
+    {
+        if(m > n)
+        {
+            m = m - n;
+        }
+        else if(n > m)
+        {
+            n = n - m;
+        }
+    }
+    cout<<"GCD is :"<<m;
+    return 0;
+}
