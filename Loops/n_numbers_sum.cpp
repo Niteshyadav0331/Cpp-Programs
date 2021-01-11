b223d63a186313b6617494e683094f30082663eb
Nitesh Yadav <55007875+Niteshyadav0331@users.noreply.github.com>
2021-01-11 11:18:15 +0530

    Add files via upload

---
+++ Loops/n_numbers_sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+    for(int i = 0;i<=10;i++)
+    {
+        sum += i;
+    }
+    cout<<"Sum is "<<sum;
+    return 0;
+}
